b51c1bcbdfb16c2678d72fdab5a7f5ae7fd91a71
Zoe Tan <zoetanxy@gmail.com>
2020-10-12 14:43:30 +0100

    add rough function test

--- gpyopt_test.py
+++ gpyopt_test.py
@@ -6,6 +6,8 @@ import time
 
 
 def f(X):
+    #Booth function
+    #optimum at X=[1,3] f(X)=0
     x = X[:,0]
     y = X[:,1]
     return((x+2*y-7)**2+(2*x+y-5)**2)
@@ -30,6 +32,8 @@ myOpt.plot_convergence()
 n=10
 
 def f_high(X):
+    #Sphere function
+    #optimum at X = [0,...,0], f(X)=0
     return(np.sum(np.power(X,2), 1))
 
 bounds = [{'name': 'var_1', 'type': 'continuous', 'domain': (-1,1)}]*n
@@ -44,3 +48,23 @@ print(toc-tic)
 print(my_n_Opt.x_opt, my_n_Opt.fx_opt)
 
 my_n_Opt.plot_convergence()
+
+#n-dim example with local minima
+
+def f_high_rough(X):
+    #Rastrigin function
+    #optimum at X=[0,...,0], f(X)=0
+    return(10*n+np.sum(np.power(X,2)-10*np.cos(2*np.pi*X),1))
+
+bounds = [{'name': 'var_1', 'type': 'continuous', 'domain': (-5.12,5.12)}]*n
+
+my_n_rough_Opt = BayesianOptimization(f_high, domain=bounds)
+
+tic = time.perf_counter()
+my_n_rough_Opt.run_optimization(max_iter = 40, max_time = 60, eps=10e-06)
+toc = time.perf_counter()
+print(toc-tic)
+
+print(my_n_rough_Opt.x_opt, my_n_rough_Opt.fx_opt)
+
+my_n_rough_Opt.plot_convergence()
