1d31e2403f74fb6ebc005542783a5ca6f5591052
Zoe Tan <zoetanxy@gmail.com>
2020-10-25 19:27:36 +0000

    rewrite testing to more easily iterate, get results for 1-D function

--- gpyopt_test.py
+++ gpyopt_test.py
@@ -1,70 +1,61 @@
 from GPyOpt.methods import BayesianOptimization
 import numpy as np
+from numpy.random import randn
 import time
+import pandas as pd
 
-#2-Dimensional Example
+np.random.seed(24)
+
+results = pd.read_excel('gpyopt_test_results.xlsx')
+
+def one_dim(x):
+    #1-D
+    #optimum at x=0.96609, f(x)=-1.48907
+    return -(1.4-3*x)*np.sin(18*x)+randn()*sigma
+bounds_one_dim = [{'name':'x', 'type':'continuous', 'domain':(0, 1.2)}]
 
 
-def f(X):
-    #Booth function
+def booth(X):
+    #2-D
     #optimum at X=[1,3] f(X)=0
     x = X[:,0]
     y = X[:,1]
-    return((x+2*y-7)**2+(2*x+y-5)**2)
+    return((x+2*y-7)**2+(2*x+y-5)**2)+randn()*sigma
+bounds_booth = [{'name': 'x', 'type': 'continuous', 'domain': (-10,10)}]
 
-bounds = [{'name': 'var_1', 'type': 'continuous', 'domain': (-10,5)},
-{'name':'var_2', 'type':'continuous', 'domain':(0, 10)}]
-
-myOpt = BayesianOptimization(f, domain=bounds)
-tic = time.perf_counter()
-myOpt.run_optimization(max_iter = 15, max_time = 60, eps=10e-06)
-toc = time.perf_counter()
-print(toc-tic)
-print(myOpt.x_opt, myOpt.fx_opt)
-
-myOpt.plot_acquisition()
-
-myOpt.plot_convergence()
-
-
-#n-dim example
-
-n=10
-
-def f_high(X):
-    #Sphere function
+def sphere(X):
+    #n-D
     #optimum at X = [0,...,0], f(X)=0
-    return(np.sum(np.power(X,2), 1))
+    return(np.sum(np.power(X,2), 1))+randn()*sigma
+bounds_sphere = [{'name': 'x', 'type': 'continuous', 'domain': (-1,1)}]
 
-bounds = [{'name': 'var_1', 'type': 'continuous', 'domain': (-1,1)}]*n
-
-my_n_Opt = BayesianOptimization(f_high, domain=bounds)
-
-tic = time.perf_counter()
-my_n_Opt.run_optimization(max_iter = 15, max_time = 60, eps=10e-06)
-toc = time.perf_counter()
-print(toc-tic)
-
-print(my_n_Opt.x_opt, my_n_Opt.fx_opt)
-
-my_n_Opt.plot_convergence()
-
-#n-dim example with local minima
-
-def f_high_rough(X):
-    #Rastrigin function
+def rastrigin(X):
+    #n-D
     #optimum at X=[0,...,0], f(X)=0
-    return(10*n+np.sum(np.power(X,2)-10*np.cos(2*np.pi*X),1))
+    return(10*len(X)+np.sum(np.power(X,2)-10*np.cos(2*np.pi*X),1))+randn()*sigma
+bounds_rastrigin = [{'name': 'x', 'type': 'continuous', 'domain': (-5.12,5.12)}]
 
-bounds = [{'name': 'var_1', 'type': 'continuous', 'domain': (-5.12,5.12)}]*n
+n = 1
+function = one_dim
+domain = bounds_one_dim*n
+actual_x = [0.96609]
+actual_f = -1.48907
+max_time = 240
 
-my_n_rough_Opt = BayesianOptimization(f_high, domain=bounds)
+for sigma in [0, 0.1, 1, 2, 4]:
+    for max_iter in [10, 20, 50, 100]:
+        for acq_type in ['EI', 'MPI', 'LCB']:
+            myOpt = BayesianOptimization(function,
+                                        domain=domain,
+                                        acquisition_type=acq_type,
+                                        exact_feval=True)
+            myOpt.run_optimization(max_iter = max_iter, max_time = 240, eps=1e-6)
+            result = {"n": n,"function":"one_dim", "sigma": sigma, "max_it": max_iter, "max_time": max_time, "acquisition func": acq_type, 
+                    "eucl_dist to true x_opt": np.linalg.norm(myOpt.x_opt-actual_x), 
+                    "diff to true f(x_opt)": np.abs(function(myOpt.x_opt)-actual_f), "actual_it": myOpt.num_acquisitions,
+                    "actual_time": myOpt.cum_time}
 
-tic = time.perf_counter()
-my_n_rough_Opt.run_optimization(max_iter = 40, max_time = 60, eps=10e-06)
-toc = time.perf_counter()
-print(toc-tic)
+            results = results.append(result, ignore_index=True)
+            print(myOpt.x_opt, result)
+results.to_excel("gpyopt_test_results.xlsx")
 
-print(my_n_rough_Opt.x_opt, my_n_rough_Opt.fx_opt)
-
-my_n_rough_Opt.plot_convergence()
